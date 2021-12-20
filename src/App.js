852247ceb3285c11ddc05b023037acb47dc4a101
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-12-20 20:21:06 +0300

    feat: add more fields to repo items

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ function App() {
       full_name: item.full_name,
       description: item.description,
       updated_at: item.updated_at,
-      language: item.language
+      language: item.language,
+
+      owner: {
+        avatar_url: item.owner.avatar_url,
+        id:  item.owner.id,
+        login:  item.owner.login,
+        html_url:  item.owner.html_url
+      },
+      private: item.private,
+      html_url: item.html_url
     })));
   };
 
