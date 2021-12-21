90ed28d1f58c578ec60864d46b160d9bade083bc
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-12-21 12:20:27 +0300

    feat: implement searchRepoById method

--- src/api/index.js
+++ src/api/index.js
@@ -9,19 +9,55 @@ export async function searchRepos(terms, page = 1, itemsPerPage = 30) {
   else if(response.status === 403) throw new Error('API rate limit exceeded :(');
   else if (response.status !== 200) throw new Error(':(');
     
+
+  console.log(data);
   return data.items.map(item => ({
     id: item.id,
     fullName: item.full_name,
     description: item.description,
     updatedAt: item.updated_at,
     language: item.language,
-    owner: {
-      avatarUrl: item.owner.avatar_url,
-      id: item.owner.id,
-      login: item.owner.login,
-      htmlUrl: item.owner.html_url
-    },
-    private: item.private,
-    htmlUrl: item.html_url
   }));
+} 
+
+
+export async function searchRepoById(repoId) {
+  const url = encodeURI(`https://api.github.com/repositories/${repoId}`);
+
+  const response = await fetch(url);
+  const data = await response.json();  
+
+  if(response.status === 403) throw new Error('API rate limit exceeded :(');
+  else if(response.status === 404) throw new Error('Not Found :(');
+  else if (response.status !== 200) throw new Error(':(');
+    
+  console.log(data);
+  const langsUrl = data.languages_url;
+  const langsResponse = await fetch(langsUrl);
+  const langs = await langsResponse.json();  
+
+  return {
+    id: data.id,
+    name: data.name,
+    fullName: data.full_name,
+    private: data.private,
+    owner: {
+      login: data.owner.login,
+      id: data.owner.id,
+      avatarUrl: data.owner.avatar_url,
+      htmlUrl: data.owner.html_url,
+    },
+    htmlUrl: data.html_url,
+    description: data.description,
+    createdAt: data.created_at,
+    updatedAt: data.updated_at,
+    pushedAt: data.pushed_at,
+    languages: langs,
+    //"languages_url": "https://api.github.com/repos/angular/angular/languages",
+    stargazersCount: data.stargazers_count,
+    watchersCount: data.watchers_count,
+    forksCount: data.forks_count,
+    topics: data.topics,//array
+    visibility: data.visibility,
+  };
 } 
