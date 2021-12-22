e6cc33420fda1a32d7e146d575c8730691fe2b51
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-12-22 14:02:39 +0300

    feat: add filters (by stars and forks count)

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 
-export async function searchRepos(terms, page = 1, itemsPerPage = 30) {
-  const url = encodeURI(`https://api.github.com/search/repositories?page=${page}&per_page=${itemsPerPage}&q=${terms}`);
-
+export async function searchRepos(terms, filter, page = 1, itemsPerPage = 30) {
+  const url = encodeURI(`https://api.github.com/search/repositories?page=${page}&per_page=${itemsPerPage}&q=${terms}${filter !== "no filter" ? ("&sort=" + filter) : ""}`);
+  //console.log(url);
   const response = await fetch(url);
-  const data = await response.json();  
+  const data = await response.json();
 
   if (response.status === 422) throw new Error('Validation Failed :(');
-  else if(response.status === 403) throw new Error('API rate limit exceeded :(');
+  else if (response.status === 403) throw new Error('API rate limit exceeded :(');
   else if (response.status !== 200) throw new Error(':(');
 
   //console.log(data);
@@ -19,22 +19,22 @@ export async function searchRepos(terms, page = 1, itemsPerPage = 30) {
     stargazersCount: item.stargazers_count,
     forksCount: item.forks_count
   }));
-} 
+}
 
 export async function searchRepoById(repoId) {
   const url = encodeURI(`https://api.github.com/repositories/${repoId}`);
 
   const response = await fetch(url);
-  const data = await response.json();  
+  const data = await response.json();
 
-  if(response.status === 403) throw new Error('API rate limit exceeded :(');
-  else if(response.status === 404) throw new Error('Not Found :(');
+  if (response.status === 403) throw new Error('API rate limit exceeded :(');
+  else if (response.status === 404) throw new Error('Not Found :(');
   else if (response.status !== 200) throw new Error(':(');
-    
+
   //console.log(data);
   const langsUrl = data.languages_url;
   const langsResponse = await fetch(langsUrl);
-  const langs = await langsResponse.json();  
+  const langs = await langsResponse.json();
 
   return {
     id: data.id,
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -3,36 +3,84 @@ import React, { useEffect, useState, useRef } from 'react';
 import { searchRepos } from '../../api'
 
 function SearchPanel({ setData, setError }) {
-  const [value, setValue] = useState('');
+  const [formState, setFormState] = useState({
+    value: "",
+    filter: 'no filter'
+  });
 
   const input = useRef(null);
 
   const setDataByTerms = async _ => {
     try {
-      setData(value !== '' ? await searchRepos(value) : []);
+      setData(formState.value !== '' ? await searchRepos(formState.value, formState.filter) : []);
     } catch (e) {
       setError(e.message);
     }
   }
 
-  const handleChange = event => {
-    setValue(event.target.value);
+  const handleInputChange = event => {
+    const target = event.target;
+    const value = target.type === 'radio' ? target.value : target.value;
+    const name = target.name;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
   }
 
   const handleSubmit = event => {
-    setDataByTerms(value);
+    setDataByTerms(formState.value);
     event.preventDefault();
     input.current.blur();
   }
 
   useEffect(() => {
-    setDataByTerms(value);
-  }, [value])
+    setDataByTerms(formState.value);
+  }, [formState])
 
   return (
     <div className="search-panel">
       <form className='search-form' onSubmit={handleSubmit}>
-        <input ref={input} className='search-form__input' value={value} onChange={handleChange} type='text' placeholder='enter the search terms...' />
+        <input
+          name="value"
+          ref={input}
+          className='search-form__input'
+          value={formState.value}
+          onChange={handleInputChange}
+          type='text'
+          placeholder='enter the search terms...' />
+        <div className="filters">
+          <label>
+            <input
+              name="filter"
+              type="radio"
+              value={'stars'}
+              checked={formState.filter === 'stars'}
+              onChange={handleInputChange}
+            />
+            filter by stars
+          </label>
+          <label>
+            <input
+              name="filter"
+              type="radio"
+              value={'forks'}
+              checked={formState.filter === 'forks'}
+              onChange={handleInputChange}
+            />
+            filter by forks
+          </label>
+          <label>
+            <input
+              name="filter"
+              type="radio"
+              value={'no filter'}
+              checked={formState.filter === 'no filter'}
+              onChange={handleInputChange}
+            />
+            no filter
+          </label>
+        </div>
       </form>
     </div>
   );
