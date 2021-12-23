3efef6a03c8427a7efd1aa0f6128582bf9de87f1
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-12-23 13:11:21 +0300

    feat: add styles to loader(Spiller)

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,22 @@
 import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
 
 function Spinner(props) {
   const { promiseInProgress } = usePromiseTracker({ area: props.area });
 
   return (
     (promiseInProgress === true) ?
-      <h3 style={{ color: "red" }}>Loading...</h3>
+      <div
+        style={{
+          width: "100%",
+          height: "100",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Loader type="ThreeDots" color="#5580de" height="100" width="100" />
+      </div>
       :
       null
   );
